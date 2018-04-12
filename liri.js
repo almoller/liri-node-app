9d3fb5b832dde12ae7a922f74850345e9f04723e
almoller <moller.alan@gmail.com>
2018-04-12 16:24:27 -0600

    Movie Functionality almost complete

--- liri.js
+++ liri.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var fs = require("fs");
+var request = require("request");
 
-console.log(keys);
+//console.log(keys);
 
 
 var task = process.argv[2];
-var title = process.argv[3];
+var title = process.argv;
 
 switch (task) {
     case "my-tweets":
@@ -37,7 +38,30 @@ function song() {
 }
 
 function movie() {
-    console.log("\nI like movies");
+    var movieTitle = "";
+
+    for (i=3; i < title.length; i++) {
+        if (i>3 && i< title.length){
+            movieTitle = movieTitle + "+" + title[i];
+        } else {
+            movieTitle += title[i];
+        }
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var movieData = JSON.parse(body);
+            console.log("\nMovie Title: " + movieData.Title);
+            console.log("Release Year: " + movieData.Year);
+            console.log("Rated: " + movieData.Rated);
+            console.log("Rotten Tomatoes Rating: " + movieData.Ratings[1].Value);
+            console.log("Produced in: " + movieData.Country);
+            console.log("Language: " + movieData.Language);
+            console.log("Plot: " + movieData.Plot);
+        }
+    });
 }
 
 function txtTask() {
